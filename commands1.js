a809d265b45ef950fec0139f7f903883a8aa8e5d
THEOD1 <131455600+THEOD1@users.noreply.github.com>
2023-04-21 19:37:23 +0800

    Add files via upload

---
+++ commands1.js
@@ -0,0 +1,89 @@
+var youtube = "https://www.youtube.com/@theod7664/";
+var twitter = "";
+var password = "fkcodes";
+var linkedin = "";
+var instagram = "https://www.instagram.com/th30d__/";
+var github = "";
+var email = 'mailto:mint.h@yahoo.com';
+
+whois = [
+  "<br>",
+  "Our team members: 1. B210970093 O.Dulguun",
+  "                  2. B210970102 Ya.Namuunaa",
+  "                  3. B210970301 M.Munkhtuguldur",
+  "                  4. B210970107 Z.Jawkhlanbayr",
+  "                  5. B210970111 N.Uynga",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/THEOD' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/th30d__' + '</a>',
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "1.ip",
+  "2.cryptography",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">register</span>       welcome to our page!',
+  '<span class="command">login</span>          oh again, it is you',
+  '<span class="command">password</span>       Find the password',
+  '<span class="command">whois</span>          Who is Team Linux?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index"></span>',
+ 
+"██╗░░░░░██╗███╗░░██╗██╗░░░██╗██╗░░██╗  ████████╗███████╗░█████╗░███╗░░░███╗",
+"██║░░░░░██║████╗░██║██║░░░██║╚██╗██╔╝  ╚══██╔══╝██╔════╝██╔══██╗████╗░████║",
+"██║░░░░░██║██╔██╗██║██║░░░██║░╚███╔╝░  ░░░██║░░░█████╗░░███████║██╔████╔██║",
+"██║░░░░░██║██║╚████║██║░░░██║░██╔██╗░  ░░░██║░░░██╔══╝░░██╔══██║██║╚██╔╝██║",
+"███████╗██║██║░╚███║╚██████╔╝██╔╝╚██╗  ░░░██║░░░███████╗██║░░██║██║░╚═╝░██║",
+"╚══════╝╚═╝╚═╝░░╚══╝░╚═════╝░╚═╝░░╚═╝  ░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+password = [
+  "<br>",
+  "1.ip",
+  "2.cryptography",
+  "<br>"
+];
+register = [
+  "<br>",
+  "1.ip",
+  "2.cryptography",
+  "<br>"
+];
+
+login = [
+  "<br>",
+  "1.ip",
+  "2.cryptography",
+  "<br>"
+];
